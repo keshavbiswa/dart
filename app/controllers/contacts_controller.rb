11787d19946a179a1dc841a423768119c3dfab72
Keshav Biswa <keshavbiswa21@gmail.com>
2019-05-04 17:10:17 +0530

    Created contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,25 @@
 class ContactsController < ApplicationController
+  
+  def new
+    @contact = Contact.new
+  end
+  
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save!
+      redirect_to contact_path(@contact)
+    else
+      render :new
+    end
   end
 
   def show
+    
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:id, :number, :country)
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
   validates :number, :country, presence: true
+
+  after_create :search
+
+  private
+
+  def search
+    # todo
+  end
 end
